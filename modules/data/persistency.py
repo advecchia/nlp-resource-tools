d674e67ff03eed202758c333b8373ea508b91073
advecchia <simagicall@gmail.com>
2015-11-11 23:41:31 -0200

    Constructed FileMapper and corrected foreign key for the Models

--- modules/data/persistency.py
+++ modules/data/persistency.py
@@ -3,12 +3,11 @@
 import logging as log
 import pickle
 import json
-from storage import Files, Paragraphs, Sentences, Tokens, database
+from modules.data.storage import Files, Paragraphs, Sentences, Tokens, database
 from src.storage.file import File
 from src.storage.paragraph import Paragraph
 from src.storage.sentence import Sentence
 from src.storage.token import Token
-from zim.stores import files
 
 class DataPersistency(object):
     def __init__(self):
@@ -35,30 +34,60 @@ class DataPersistency(object):
         log.info('{}: end.'.format(method_name))
         return dump
 
-    def save_json_to_file(self):
-        pass
-    def read_json_from_file(self):
-        pass
+    def save_json_to_file(self, data, filename):
+        method_name = "DataPersistency.save_json_to_file"
+        log.info('{}: initialization.'.format(method_name))
+        with open(filename, 'wb') as o:
+            o.write(self.encode_string_to_json(data))
+            o.close()
+        log.info('{}: end.'.format(method_name))
+
+    def read_json_from_file(self, filename):
+        method_name = "DataPersistency.read_json_from_file"
+        log.info('{}: initialization.'.format(method_name))
+        with open(filename, 'rb') as i:
+            data = i.read()
+            i.close()
+        log.info('{}: end.'.format(method_name))
+        return self.decode_string_from_json(data)
 
     def encode_string_to_json(self, data):
+        method_name = "DataPersistency.encode_string_to_json"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
         return json.dumps(data)
 
     def decode_string_from_json(self, data):
+        method_name = "DataPersistency.decode_string_from_json"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
         return json.loads(data)
 
-    def encode_file_to_json(self, filename):
+    def encode_file_to_json_stream(self, filename):
+        method_name = "DataPersistency.encode_file_to_json_stream"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
         return json.dump(filename)
 
-    def decode_file_from_json(self, filename):
+    def decode_file_from_json_stream(self, filename):
+        method_name = "DataPersistency.decode_file_from_json_stream"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
         return json.load(filename)
     
     def before_request_handler(self):
+        method_name = "DataPersistency.before_request_handler"
+        log.info('{}: initialization.'.format(method_name))
         if not self._connection:
             self._database.connect()
+        log.info('{}: end.'.format(method_name))
     
     def after_request_handler(self):
+        method_name = "DataPersistency.after_request_handler"
+        log.info('{}: initialization.'.format(method_name))
         if self._connection:
             self._database.close()
+        log.info('{}: end.'.format(method_name))
 
 class FileMapper(object):
     def __init__(self):
@@ -66,47 +95,68 @@ class FileMapper(object):
         log.info('{}: initialization.'.format(method_name))
         log.info('{}: end.'.format(method_name))
     
-    def file_to_db_insert(self, file):
+    def file_to_db_insert(self, file_object):
         method_name = "FileMapper.file_to_db_insert"
         log.info('{}: initialization.'.format(method_name))
-        query = Files.insert(parallel_file=file.parallel_file.id, name=file.name, 
-                             size=file.size, modified=file.modified, language=file.language)
-        file.id = query.execute() # Return new id
+        query = Files.insert(parallel_file=file_object.parallel_file.id, name=file_object.name, 
+                             size=file_object.size, modified=file_object.modified, language=file_object.language)
+        file_object.id = query.execute() # Return new id
         log.info('{}: end.'.format(method_name))
-        return file
+        return file_object
 
-    def file_to_db_update(self, file):
+    def file_to_db_update(self, file_object):
         method_name = "FileMapper.file_to_db_update"
         log.info('{}: initialization.'.format(method_name))
-        query = Files.update(parallel_file=file.parallel_file.id, name=file.name, 
-                             size=file.size, modified=file.modified, language=file.language).where(id==file.id)
+        query = Files.update(parallel_file_id=file_object.parallel_file.id, name=file_object.name, 
+                         size=file_object.size, modified=file_object.modified, language=file_object.language).where(id==file_object.id)
         log.info('{}: end.'.format(method_name))
-        if query.execute() > 0: return True
+        if query.execute() != 1: return True
         else: return False
 
     def db_to_file_get_files(self):
         method_name = "FileMapper.db_to_file_get_files"
         log.info('{}: initialization.'.format(method_name))
         files = []
-        for file in Files.select():
-            files.append(self.mapper_db_to_file(file));
+        for db_file in Files.select():
+            files.append(self.mapper_db_to_file(db_file));
         log.info('{}: end.'.format(method_name))
         return files
 
-    def db_to_file_get_file(self, id):
+    def db_to_file_get_file(self, file_id):
         method_name = "FileMapper.db_to_file_get_file"
         log.info('{}: initialization.'.format(method_name))
+        
         try:
-            file = Files.get(Files.id==id)
+            file_object = Files.get(Files.id==file_id)
             log.info('{}: end.'.format(method_name))
-            return self.mapper_db_to_file(file)
-        except FilesDoesNotExist as e:
-            log.info('FilesDoesNotExist {}: {}.'.format(e.errno, e.strerror))
+            return self.mapper_db_to_file(file_object)
+        except Exception as e:
+            log.info('FilesDoesNotExist errno={}: strerror{}.'.format(e.errno, e.strerror))
             log.info('{}: end.'.format(method_name))
-            
+            raise Exception('FilesDoesNotExist errno={}: strerror{}.'.format(e.errno, e.strerror))
+
     def mapper_db_to_file(self, db_file):
         method_name = "FileMapper.mapper_db_to_file"
         log.info('{}: initialization.'.format(method_name))
-        return File(id==db_file.id, parallel_file_id=db_file.parallel_file, name=db_file.name, 
+        log.info('{}: end.'.format(method_name))
+        return File(id=db_file.id, parallel_file_id=db_file.parallel_file_id, name=db_file.name, 
                              size=db_file.size, modified=db_file.modified, language=db_file.language)
+
+# TODO: implement the below classes
+class ParagraphMapper(object):
+    def __init__(self):
+        method_name = "ParagraphMapper"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
+
+class SentenceMapper(object):
+    def __init__(self):
+        method_name = "SentenceMapper"
+        log.info('{}: initialization.'.format(method_name))
+        log.info('{}: end.'.format(method_name))
+
+class TokenMapper(object):
+    def __init__(self):
+        method_name = "TokenMapper"
+        log.info('{}: initialization.'.format(method_name))
         log.info('{}: end.'.format(method_name))
--- modules/data/storage.py
+++ modules/data/storage.py
@@ -1,48 +1,48 @@
-import logging as log
-from peewee import *
+import peewee as pw
 
-database = MySQLDatabase('mestrado', **{'password': 'mestrado', 'user': 'mestrado'})
+database = pw.MySQLDatabase('mestrado', **{'password': 'mestrado', 'user': 'mestrado'})
 
-class BaseModel(Model):
+class BaseModel(pw.Model):
     class Meta:
         database = database
 
 class Files(BaseModel):
-    language = CharField(null=True)
-    modified = DateTimeField()
-    name = CharField(unique=True)
-    parallel_file = ForeignKeyField(db_column='parallel_file_id', rel_model='self', to_field='id')
-    size = IntegerField()
+    id = pw.IntegerField(db_column='id', primary_key=True)
+    language = pw.CharField(null=True, max_length=25)
+    modified = pw.DateTimeField()
+    name = pw.CharField(unique=True, max_length=150)
+    parallel_file_id = pw.ForeignKeyField(db_column='parallel_file_id', rel_model='self', to_field='id')
+    size = pw.IntegerField()
 
     class Meta:
         db_table = 'files'
 
 class Paragraphs(BaseModel):
-    file = ForeignKeyField(db_column='file_id', rel_model=Files, to_field='id')
-    order_in_file = IntegerField()
+    file = pw.ForeignKeyField(db_column='file_id', rel_model=Files, to_field='id')
+    order_in_file = pw.IntegerField()
 
     class Meta:
         db_table = 'paragraphs'
 
 class Sentences(BaseModel):
-    number_of_words = IntegerField()
-    order_in_paragraph = IntegerField()
-    paragraph = ForeignKeyField(db_column='paragraph_id', rel_model=Paragraphs, to_field='id')
+    number_of_words = pw.IntegerField()
+    order_in_paragraph = pw.IntegerField()
+    paragraph = pw.ForeignKeyField(db_column='paragraph_id', rel_model=Paragraphs, to_field='id')
 
     class Meta:
         db_table = 'sentences'
 
 class Tokens(BaseModel):
-    id = BigIntegerField(primary_key=True)
-    lemma = TextField()
-    length = IntegerField()
-    order_in_sentence = IntegerField()
-    pos = CharField()
-    sentence = ForeignKeyField(db_column='sentence_id', rel_model=Sentences, to_field='id')
-    syllables = TextField()
-    synonyms = TextField(null=True)
-    tf_idf = FloatField(null=True)
-    token = TextField()
+    id = pw.BigIntegerField(primary_key=True)
+    lemma = pw.TextField()
+    length = pw.IntegerField()
+    order_in_sentence = pw.IntegerField()
+    pos = pw.CharField()
+    sentence = pw.ForeignKeyField(db_column='sentence_id', rel_model=Sentences, to_field='id')
+    syllables = pw.TextField()
+    synonyms = pw.TextField(null=True)
+    tf_idf = pw.FloatField(null=True)
+    token = pw.TextField()
 
     class Meta:
         db_table = 'tokens'
